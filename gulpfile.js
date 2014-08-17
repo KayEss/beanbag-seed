7fd5f372c29555c5cf45a0d2b330827a1df6fe5f
Kirit Sælensminde <k@kirit.com>
2014-08-17 12:21:11 +0700

    Compile the bootstrap CSS with less.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
 gulp.task('libs', function() {
-    gulp.src('bower_components/jquery/dist/jquery.js').
+    gulp.
+        src('bower_components/jquery/dist/jquery.js').
         pipe(gulp.dest('www/lib'));
+});
+
+gulp.task('less', function() {
+    gulp.
+        src('css/*.less').
+        pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        })).
+        pipe(gulp.dest('www/css'));
 });
 
 gulp.task('watch', function() {
     gulp.watch('js/*.js', []);
+    gulp.watch('css/*.less', ['less']);
 });
 
-gulp.task('default', ['libs', 'watch']);
+gulp.task('default', ['libs', 'less', 'watch']);
