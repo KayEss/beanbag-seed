03cce50bd0b3455fd0888aaf6cca9f8d5dd95a15
Kirit Sælensminde <k@kirit.com>
2014-08-22 10:43:00 +0700

    Don't lazy load the controller as it breaks the tests.

--- js/app/app.js
+++ js/app/app.js
@@ -1,11 +1,5 @@
-define(['angular'], function(angular) {
+define(['angular', 'app/Page'], function(angular, Page) {
     angular.module('app', []).
-        controller("Page", ['$scope', function() {
-            var self = this, args = arguments;
-            require(['app/Page'], function(controller) {
-                var $rootScope = angular.element('html').scope();
-                $rootScope.$apply(function() {controller.apply(self, args);});
-            });
-        }]);
+        controller("Page", ['$scope', Page]);
     angular.bootstrap(document, ['app']);
 });
